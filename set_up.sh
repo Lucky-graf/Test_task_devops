48879f73dc952c10db2f682d61bf50c0bd12e525
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2022-06-13 16:53:41 +0300

    Update set_up.sh

--- set_up.sh
+++ set_up.sh
@@ -5,11 +5,10 @@ npm install -g npm@latest
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 source ~/.bashrc
-nvm list-remote
 nvm install --lts
 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get update
 apt-get install yarn -y
 
@@ -40,5 +39,3 @@ curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compo
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
-sudo usermod -aG docker ${USER}
-newgrp docker
